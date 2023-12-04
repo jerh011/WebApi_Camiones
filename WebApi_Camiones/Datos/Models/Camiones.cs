4e0e54c2a9de0aa7a45d603c46ea2e1a864736a8
jerh011 <jeelrohe@gmail.com>
2023-12-03 19:47:58 -0700

    primera_relacion(camion_camionero) antes del Add-Migration [Nombre de la migracion]

---
+++ WebApi_Camiones/Datos/Models/Camiones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace WebApi_Camiones.Datos.Models
+{
+    public class Camiones
+    {
+        public string Id { get; set; }
+        public string Placas { get; set; }
+        public string Modelo {  get; set; }
+        public List<Camionero_Camiones> Camionero_Camion { get; set; }
+    }
+}
